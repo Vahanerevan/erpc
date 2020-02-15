88ecce09cb908d2b424eed30b7441ce48b588d21
Vahan Mkrtchyan <vahan.mkrtchyan@atlas-iac.com>
2020-02-15 15:55:43 +0200

    change action structure

--- request.go
+++ request.go
@@ -2,6 +2,8 @@ package erpc
 
 import (
 	"encoding/json"
+	"fmt"
+
 	//"encoding/json"
 	"errors"
 	"github.com/imroc/req"
@@ -32,33 +34,6 @@ func (request *Request) SetRequestObject(dataObject interface{}) {
 	}
 }
 
-//func (request *Request) Call(requestObject interface{}, path ...string) (*Response, error) {
-//	request.SetRequestObject(requestObject)
-//	header := req.Header{
-//		"Content-Type": "application/json",
-//	}
-//	pathList := []string{request.config.URL}
-//	pathList = append(pathList, path...)
-//	uri := strings.Join(pathList, "/")
-//	r, err := req.Post(uri, header, req.BodyJSON(&request.payload))
-//
-//	if nil != err {
-//		return nil, err
-//	}
-//
-//	resp := r.Response()
-//
-//	if resp.StatusCode != http.StatusOK {
-//		return nil, errors.New("Response status is not 200")
-//	}
-//
-//	responseData := &request.Resp
-//	r.ToJSON(responseData)
-//	responseData.Resp = r
-//
-//	return responseData, nil
-//}
-
 func (request *Request) Action(action string, requestObject interface{}, path ...string) (*Response, error) {
 
 	request.SetRequestObject(requestObject)
@@ -77,6 +52,8 @@ func (request *Request) Action(action string, requestObject interface{}, path ..
 
 	uri := strings.Join(pathList, "/")
 
+	fmt.Println(uri)
+
 	r, err := req.Post(uri, header, bytes)
 
 	if nil != err {
@@ -90,7 +67,9 @@ func (request *Request) Action(action string, requestObject interface{}, path ..
 	}
 
 	responseData := &request.Resp
+
 	r.ToJSON(responseData)
+
 	responseData.Resp = r
 
 	return responseData, nil
