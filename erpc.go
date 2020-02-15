cffb530408ad8c1bc4496e8729c6bc7344479029
Vahan Mkrtchyan <vahan.mkrtchyan@atlas-iac.com>
2020-02-15 19:26:42 +0200

    change action structure

--- erpc.go
+++ erpc.go
@@ -4,8 +4,10 @@ const StatusOK = "OK"
 const StatusFAIL = "FAIL"
 const XHeader string = "X-Auth"
 
-
-
 func IsStatusOK(status string) bool {
 	return status == StatusOK
+}
+
+func ValidateInput(secret, hash string, input []byte) bool {
+	return hash == HashCalculate(input, secret)
 }
